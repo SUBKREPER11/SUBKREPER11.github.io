1a69b75f04f74480d3866bae2ff89483210f4385
SUBKREPER11 <jakub.olejnik2004@wp.pl>
2023-05-29 11:26:10 +0200

    dec2bin

--- Addons/dec2bin.js
+++
@@ -1,5 +0,0 @@
-function calc(){
-    var num = document.getElementById("num").value;
-    // console.log(typeof(Number(num)))
-    document.getElementById("res").innerHTML = Number(num).toString(2);
-}
