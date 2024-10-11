c94a2529a1ca9489765d5e139099c0ab413ee567
SUBKREPER11 <80513311+SUBKREPER11@users.noreply.github.com>
2024-10-11 21:30:38 +0200

    PWAUpdate

---
+++ PWAApp/app.js
@@ -0,0 +1,10 @@
+console.log('Aplikacja PWA działa!');
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker zarejestrowany:', registration.scope);
+        })
+        .catch((error) => {
+            console.log('Błąd rejestracji Service Workera:', error);
+        });
+}
---
+++ PWAApp/service-worker.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = 'moja-pwa-cache';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js',
+    '/addons/clock.js'
+];
+
+// Instalacja Service Workera i cache'owanie zasobów
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting(); // Natychmiast aktywuj nową wersję
+});
+
+// Aktywacja i czyszczenie starych cache'y
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Usunięto stary cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim(); // Od razu aktywuj nową wersję dla wszystkich klientów
+});
+
+// Fetch - strategia cache-first
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    // Zasób jest w cache, zwróć go
+                    return response;
+                }
+                // Zasób nie jest w cache, pobierz z sieci
+                return fetch(event.request).then((networkResponse) => {
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        // Zapisz nowy zasób w cache na przyszłość
+                        cache.put(event.request, networkResponse.clone());
+                        return networkResponse;
+                    });
+                });
+            })
+    );
+});
+
+// Sprawdzenie i aktualizacja cache'owanych zasobów w tle
+self.addEventListener('sync', (event) => {
+    if (event.tag === 'sync-cache') {
+        event.waitUntil(
+            caches.open(CACHE_NAME).then((cache) => {
+                return fetch('/').then((response) => {
+                    cache.put('/', response);
+                });
+            })
+        );
+    }
+});
